6ad8e9befced75dd5463f0a034948f94a6cd46af
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-24 15:49:30 +0100

    add static header + main

---
+++ src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <a href="">
+            <img src="../assets/img/dc-logo.png" alt="DC Logo">
+        </a>
+        <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li class="active"><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto;
+    img {
+        padding: 20px 0;
+    }
+    li {
+        display: inline-block;
+        margin: 0 15px;
+        padding: 60px 0;
+        a {
+            text-decoration: none;
+            color: black;
+        }
+        &.active {
+            a {
+                color: blue;
+            }
+            border-bottom: 5px solid blue;
+        }
+        &:hover {
+            a {
+                color: blue;
+            }
+            border-bottom: 5px solid blue;
+        }
+    }
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <div class="container">content goes here</div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+main {
+    background-color: black;
+    color: white;
+    font-weight: bold;
+    padding: 100px 0;
+    .container {
+        width: 80%;
+        margin: 0 auto;
+    }
+}
+</style>
