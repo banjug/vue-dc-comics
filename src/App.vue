1a3590bc80d2e2619551cd74a04a7fd66caeda34
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-24 16:08:26 +0100

    add footer components

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header/>
     <Main/>
     <LinkStrip/>
+    <Footer/>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Header from './components/Header.vue'
 import Main from './components/Main.vue'
 import LinkStrip from './components/LinkStrip.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
@@ -17,6 +19,7 @@ export default {
     Header,
     Main,
     LinkStrip,
+    Footer,
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <FooterNav/>
+        <FooterLinks/>
+    </footer>
+</template>
+
+<script>
+import FooterNav from '../components/FooterNav.vue'
+import FooterLinks from '../components/FooterLinks.vue'
+export default {
+    name: 'Footer',
+    components: {
+        FooterNav,
+        FooterLinks
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
