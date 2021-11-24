d8f3454a8e4966ad3e17adb1da10b0ea6670bf74
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-24 17:06:14 +0100

    add footer nav data obj

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -5,20 +5,28 @@
                 <div>
                     <ul>
                         <h3>DC COMICS</h3>
-                        <li>Characters</li>
+                        <li v-for="link, i in dcComicsLinks" :key="i">
+                            <a href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                     <ul>
-                        <h3>DC COMICS</h3>
-                        <li>Characters</li>
+                        <h3>SHOP</h3>
+                        <li v-for="link, i in shopLinks" :key="i">
+                            <a href="link.url">{{link.text}}</a>
+                        </li>
                     </ul>
                 </div>
                 <ul>
-                    <h3>DC COMICS</h3>
-                    <li>Characters</li>
+                    <h3>DC</h3>
+                    <li v-for="link, i in dcLinks" :key="i">
+                        <a href="link.url">{{link.text}}</a>
+                    </li>
                 </ul>
                 <ul>
-                    <h3>DC COMICS</h3>
-                    <li>Characters</li>
+                    <h3>SITES</h3>
+                    <li v-for="link, i in sitesLinks" :key="i">
+                        <a href="link.url">{{link.text}}</a>
+                    </li>
                 </ul>
             </div>
             <img src="../assets/img/dc-logo-bg.png" alt="">
@@ -29,6 +37,119 @@
 <script>
 export default {
     name: 'FooterNav',
+    data(){
+        return{
+            dcComicsLinks: [
+                {
+                    url: '#',
+                    text: 'Characters'
+                },
+                {
+                    url: '#',
+                    text: 'Comics'
+                },
+                {
+                    url: '#',
+                    text: 'Movies'
+                },
+                {
+                    url: '#',
+                    text: 'TV'
+                },
+                {
+                    url: '#',
+                    text: 'Games'
+                },
+                {
+                    url: '#',
+                    text: 'Videos'
+                },
+                {
+                    url: '#',
+                    text: 'News'
+                },
+            ],
+            shopLinks: [
+                {
+                    url: '#',
+                    text: 'Shop DC'
+                },
+                {
+                    url: '#',
+                    text: 'Shop DC Collectibles'
+                },
+
+            ],
+            dcLinks: [
+                {
+                    url: '#',
+                    text: 'Terms Of Use'
+                },
+                {
+                    url: '#',
+                    text: 'Privacy Policy (New)'
+                },
+                {
+                    url: '#',
+                    text: 'Ad Choices'
+                },
+                {
+                    url: '#',
+                    text: 'Advertising'
+                },
+                {
+                    url: '#',
+                    text: 'Jobs'
+                },
+                {
+                    url: '#',
+                    text: 'Subscriptions'
+                },
+                {
+                    url: '#',
+                    text: 'Talent Workshop'
+                },
+                {
+                    url: '#',
+                    text: 'CPSC Certificates'
+                },
+                {
+                    url: '#',
+                    text: 'Ratings'
+                },
+                {
+                    url: '#',
+                    text: 'Shop Help'
+                },
+                {
+                    url: '#',
+                    text: 'Contact Us'
+                },
+            ],
+            sitesLinks: [
+                {
+                    url: '#',
+                    text: 'DC'
+                },
+                {
+                    url: '#',
+                    text: 'MAD Magazine'
+                },
+                {
+                    url: '#',
+                    text: 'DC Kids'
+                },
+                {
+                    url: '#',
+                    text: 'DC Universe'
+                },
+                {
+                    url: '#',
+                    text: 'DC Power Visa'
+                },
+            ],
+        }
+    }
 }
 </script>
 
@@ -50,6 +171,15 @@ nav {
             display: flex;
             > * {
                 margin: 0 20px;
+            }
+            li {
+                list-style: none;
+                    margin: 10px 0;
+                    font-size: .8rem;
+                a {
+                    color: white;
+                    text-decoration: none;
+                }
             }
         }
         img {
