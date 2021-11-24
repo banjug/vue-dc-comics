72f6cfeaade9b2ed514d2ea3cddef45cdaa294c3
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-24 16:39:30 +0100

    footer nav style

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,7 +1,29 @@
 <template>
-    <div>
-
-    </div>
+    <nav>
+        <div class="container">
+            <div class="link-cont">
+                <div>
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li>Characters</li>
+                    </ul>
+                    <ul>
+                        <h3>DC COMICS</h3>
+                        <li>Characters</li>
+                    </ul>
+                </div>
+                <ul>
+                    <h3>DC COMICS</h3>
+                    <li>Characters</li>
+                </ul>
+                <ul>
+                    <h3>DC COMICS</h3>
+                    <li>Characters</li>
+                </ul>
+            </div>
+            <img src="../assets/img/dc-logo-bg.png" alt="">
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -12,4 +34,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+nav {
+    background-image: url(../assets/img/footer-bg.jpg);
+    // padding: 50px 0;
+    color: white;
+    .container {
+        width: 80%;
+        height: 400px;
+        overflow: hidden;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        .link-cont {
+            padding: 30px 0;
+            display: flex;
+            > * {
+                margin: 0 20px;
+            }
+        }
+        img {
+            height: 150%;
+            object-fit: cover;
+            transform: translate(0, -20%);
+        }
+    }
+}
 </style>
