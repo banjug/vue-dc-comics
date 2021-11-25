490d060a724af340f798bae80d9b43a655d5d57b
Samuele Miotto <miotto.samuele@gmail.com>
2021-11-25 14:39:18 +0100

    add v-for in comics section

---
+++ src/components/ComicCard.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="comic-cont">
+        <a href="">
+            <img class="comic-cover" :src="details.thumb" :alt="details.series">
+            <span class="comic-title">{{details.series}}</span>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComicCard',
+    props: {
+        details: Object,
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    .comic-cont {
+        a {
+            text-decoration: none;
+        }
+        width: calc(100% / 6);
+        padding: 0 20px;
+        margin-bottom: 50px;
+        transition: all .1s ease;
+        color: white;
+        &:hover {
+            transform: scale(1.02);
+        }
+        .comic-cover {
+            width: 100%;
+            display: block;
+            aspect-ratio: 1;
+            object-fit: cover;
+            object-position: top;
+            margin-bottom: 20px;
+        }
+        .comic-title {
+            color: white;
+        }
+    }
+</style>
--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -1,63 +1,101 @@
 <template>
     <div class="container">
         <span class="series-label">CURRENT SERIES</span>
-        <div class="comic-cont">
-            <a href="">
-                <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-                <span class="comic-title">ACTION COMICS</span>
-            </a>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
-        <div class="comic-cont">
-            <img class="comic-cover" src="https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX" alt="">
-            <span class="comic-title">ACTION COMICS</span>
-        </div>
+        
+        <ComicCard
+        v-for="comic, i in comics" :key="i"
+        :details="comic"
+        />
+        
         <a href="" class="load-btn">LOAD MORE</a>
     </div>
 </template>
 
 <script>
+import ComicCard from '../components/ComicCard.vue'
 export default {
     name: 'Comics',
+    components: {
+        ComicCard,
+    },
+    data(){
+        return{
+            comics: [
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -84,27 +122,7 @@ export default {
         a {
             text-decoration: none;
         }
-        .comic-cont {
-            width: calc(100% / 6);
-            padding: 0 20px;
-            margin-bottom: 50px;
-            transition: all .1s ease;
-            color: white;
-            &:hover {
-                transform: scale(1.02);
-            }
-            .comic-cover {
-                width: 100%;
-                display: block;
-                aspect-ratio: 1;
-                object-fit: cover;
-                object-position: top;
-                margin-bottom: 20px;
-            }
-            .comic-title {
-                color: white;
-            }
-        }
+        
         .load-btn {
             margin: 0 auto;
             color: white;
@@ -117,5 +135,6 @@ export default {
             }
         }
     }
+    
 
 </style>
